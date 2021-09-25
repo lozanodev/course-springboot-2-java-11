a6b0cbdddedf6531e1e92fe2de452edd6d7ab98f
lozanodev <madakey@live.com>
2021-09-25 16:25:21 -0300

    Service layer, component registration

--- src/main/java/com/nomedodominio/course/resources/UserResource.java
+++ src/main/java/com/nomedodominio/course/resources/UserResource.java
@@ -1,23 +1,33 @@
 package com.nomedodominio.course.resources;
 
 import com.nomedodominio.course.entities.User;
+import com.nomedodominio.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 // Classe Controller do Programa
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping("/users")
 public class UserResource {
 
     // Get básico com instanciamento de User
+    @Autowired
+    private UserService service;
 
     @GetMapping
-    public ResponseEntity<User> findAll(){ // Response Entity é uma classe padrão do Springboot
+    public ResponseEntity<List<User>> findAll(){ // Response Entity é um método padrão do Springboot
         // Junto do parametro "FindAll()" que é utilizado para encontrar todos os usuários.
-        User u = new User(1L,"Maria","Maria@Gmail.com","888888","12345");
-        return ResponseEntity.ok().body(u); // Retornar se encontrar algo.
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list); // Retornar se encontrar algo.
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
---
+++ src/main/java/com/nomedodominio/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.nomedodominio.course.services;
+
+import com.nomedodominio.course.entities.User;
+import com.nomedodominio.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+       Optional<User> obj = repository.findById(id);
+       return obj.get();
+    }
+}
