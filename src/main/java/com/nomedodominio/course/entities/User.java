bedf9b5b866aad020238ad5eb90d074faf352ad9
lozanodev <madakey@live.com>
2021-09-25 12:06:52 -0300

    H2 database, test profile, JPA

--- src/main/java/com/nomedodominio/course/entities/User.java
+++ src/main/java/com/nomedodominio/course/entities/User.java
@@ -1,8 +1,13 @@
 package com.nomedodominio.course.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class User implements Serializable {
     private static final long serialVersionUID;
 
@@ -10,6 +15,8 @@ public class User implements Serializable {
         serialVersionUID = 1L;
     }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
